cf847e4c2dd29bf64e06435b51247f82bf9d726d
rahmat soleh <solehr619@gmail.com>
2022-12-15 12:57:28 +0700

    inital commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://gallery-app.netlify.app" // TODO: replace this
+const studentName = "Rahmat Soleh" // TODO: replace this
+const studentId = "FE3003343" // TODO: replace this
+
+export { url, studentName, studentId }
